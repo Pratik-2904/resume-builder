623ededbb3701bcc38e2a4101d0384976b39d63f
Pratik Sukale <vjkiran@MacBookPro.lan>
2025-03-14 00:35:07 +0530

    Shadcn Setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
+// import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),  // check this line if issue is there ./src cn be thesre
+    },
+  },
 })
